0846e50c119b60581fa0789b27e9397214ead3e0
Nikita Belousov <seriouscat1001@gmail.com>
2016-01-14 19:50:21 +0300

    Заебок

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 ;(function main(){
     window.onload = function() {
+
         new Game("screen");
     }
 
-    // ===================== classes =====================
+    // ========================= classes ==========================
 
     // game
 
@@ -14,41 +15,38 @@
 
         this.ctx = canvas.getContext("2d");
         this.screen_size = { width: canvas.width, height: canvas.height };
-        this.elements = [];
         this.frames = 0;
         this.sprites = {};
         this.states = {menu: 0, game: 1, game_over: 2};
         this.current_state = this.states.menu;
+        this.score = 0;
         sprite_sheet.src = "res/sheet.png";
 
         sprite_sheet.onload = function() {
             Sprite.init(sprite_sheet, self.sprites);
-            self.elements.push(new Background(self), new Player(self));
+            self.elements = {background: new Background(self), player: new Player(self), donut_field: new DonutField(self)};
             loop(this.ctx, this.screen_size);
         }
 
         var loop = function() {
             self.frames++;
             self.update();
-            self.draw(this.ctx, this.screen_size);
+            self.draw();
             requestAnimationFrame(loop);
         }
     }
 
     Game.prototype = {
         update: function() {
-            for (var i = 0; i < this.elements.length; i++) {
-                this.elements[i].update();
-            }
+            for (e in this.elements)
+                this.elements[e].update();
         },
 
-        draw: function(ctx, screen_size) {
+        draw: function() {
             this.ctx.clearRect(0, 0, this.screen_size.width, this.screen_size.height);
 
-            for (var i = 0; i < this.elements.length; i++) {
-                var e = this.elements[i];
-                e.draw(this.ctx);
-            }
+            for (e in this.elements)
+                this.elements[e].draw(this.ctx);
         }
     }
 
@@ -56,22 +54,131 @@
 
     var Player = function(game) {
         this.game = game;
+        this.lifes_max = 3;
+        this.lifes = 3;
         this.position = {x: 40, y: 0};
         this.sprite = game.sprites.player;
-        this.states = {floating: 0, falling: 1};
-        this.current_state = this.states.floating;
+        this.l_sprites = {on: game.sprites.heart, off: game.sprites.heart_off};
+        this.states = {pre_game: "pre_game", fall: "game", dead: "dead"};
+        this.current_state = this.states.pre_game;
+        this.vel = 0;
+        this.jump_h = 6;
+        this.gravity = 0.3;
+        this.rotation = 0;
+
+        var self = this;
+        
+        document.addEventListener("mousedown", function() {
+            self.handler();
+        });
     }
 
     Player.prototype = {
         update: function() {
-            if (this.current_state == this.states.floating) {
-                this.position.y = this.game.screen_size.height / 2 - this.sprite.width / 2
-                    + 8 * Math.cos(this.game.frames / 10);
+            if (this.current_state != this.states.dead) {
+                if (this.current_state == this.states.pre_game)
+                    this.position.y = this.game.screen_size.height / 2 - this.sprite.width / 2
+                        + 8 * Math.cos(this.game.frames / 10);
+                else {
+                    var rect = this.get_rect();
+                    this.vel += this.gravity;
+                    this.position.y += this.vel;
+                    if (rect.position.y + rect.height < 0 || rect.position.y > this.game.screen_size.height) {
+                        if (--this.lifes >= 0)
+                            this.current_state = this.states.pre_game;
+                        else this.current_state = this.states.dead;
+                    }
+                }
             }
         },
 
         draw: function(ctx) {
-            this.sprite.draw(ctx, this.position.x, this.position.y);
+            if (this.current_state != this.states.dead)
+                this.sprite.draw(ctx, this.position.x, this.position.y);
+
+            for (var i = 0; i < this.lifes_max; i++) {
+                this.l_sprites.off.draw(ctx, 10 + i * 24, 10);
+                if (i < this.lifes) this.l_sprites.on.draw(ctx, 10 + i * 24, 10);
+            }
+        },
+
+        handler: function() {
+            switch (this.current_state) {
+                case this.states.pre_game:
+                    this.current_state = this.states.game;
+                    this.jump();
+                    break;
+                case this.states.game:
+                    this.jump();
+                    break;
+            }
+        },
+
+        jump: function() {
+            this.vel = -this.jump_h;
+        },
+
+        get_rect: function() {
+            return new Rectangle(this.position.x, this.position.y, this.sprite.width, this.sprite.height);
+        }
+    }
+
+    // donuts
+
+    var DonutField = function(game) {
+        this.donuts = [];
+        this.intervals = [10, 20, 30, 50, 100];
+        this.interval_changing_time = 250;
+        this.speed_range = {min: 1, max: 8};
+        this.game = game;
+    }
+
+    DonutField.prototype = {
+        update: function() {
+            if ( (this.game.frames % this.interval_changing_time == 0) || (!this.interval) )
+                this.interval = get_random_array_element(this.intervals);
+            if (this.game.frames % this.interval == 0)
+                this.donuts.push(new Donut(this.game, this.speed_range));
+
+            for (var i = 0; i < this.donuts.length; i++) {
+                var d = this.donuts[i];
+                var p_rect = this.game.elements.player.get_rect();
+
+                if ( (this.game.elements.player.current_state != this.game.elements.player.states.pre_game)
+                    && (d.get_center().x > p_rect.position.x)
+                    && (d.get_center().x < p_rect.position.x + p_rect.width)
+                    && (d.get_center().y > p_rect.position.y)
+                    && (d.get_center().y < p_rect.position.y + p_rect.height) ) {
+                    d.is_eaten = true;
+                    this.game.score += d.speed;
+                }
+
+                d.position.x -= d.speed;
+                if ( (d.position.x < -d.sprite.width) || (d.is_eaten) )
+                    this.donuts.splice(i, 1);
+            }
+        },
+
+        draw: function(ctx) {
+            for (var i = 0; i < this.donuts.length; i++) {
+                var d = this.donuts[i];
+                d.sprite.draw(ctx, d.position.x, d.position.y);
+            }
+        }
+    }
+
+    var Donut = function(game, speed_range) {
+        this.sprite = game.sprites.donuts[get_random_int(0, game.sprites.donuts.length - 1)];
+        this.position = {x: game.screen_size.width,
+            y: get_random_int(0, game.screen_size.height - this.sprite.width)};
+        this.speed = get_random_int(speed_range.min, speed_range.max);
+        this.is_eaten = false;
+    }
+
+    Donut.prototype = {
+        get_center: function() {
+            return {x: this.position.x + this.sprite.width / 2,
+                y: this.position.y + this.sprite.height / 2};
         }
     }
 
@@ -101,7 +208,6 @@
             for (var i = 0; i < this.stars.length; i++) {
                 var star = this.stars[i];
                 star.update(this.speed);
-
                 if (star.position.x + star.size < 0)
                     this.stars.splice(i, 1);
             }
@@ -124,8 +230,6 @@
         }
     }
 
-    // star
-
     var Star = function(game) {
         this.game = game;
         this.size_range = {min: 2, max: 12};
@@ -145,15 +249,14 @@
 
     var Sprite = function(img, x, y, width, height) {
         this.img = img;
-        this.x = x;
-        this.y = y;
+        this.position = {x: x, y: y};
         this.width = width;
         this.height = height;
     }
 
     Sprite.prototype = {
         draw: function(ctx, x, y) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height,
+        ctx.drawImage(this.img, this.position.x, this.position.y, this.width, this.height,
             x, y, this.width, this.height);
         }
     }
@@ -212,9 +315,40 @@
         };
     }
 
+    // rectangle
+
+    var Rectangle = function(x, y, width, height) {
+        this.position = {x: x, y: y};
+        this.width = width;
+        this.height = height;
+    }
+
+    // controller 
+
+    var Controller = function() {
+        var pressed_keys = {}
+
+        window.onkeydown = function(e) {
+            pressed_keys[e.keyCode] = true;
+        }
+
+        window.onkeyup = function(e) {
+            pressed_keys[e.keyCode] = false;
+        }
+
+        this.is_down = function(code) {
+            return pressed_keys[code] === true;
+        }
+    }
+
     // ===================== helper functions =====================
 
     function get_random_int(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    function get_random_array_element(array) {
+        return array[get_random_int(0, array.length - 1)];
+    }
+
 })();
